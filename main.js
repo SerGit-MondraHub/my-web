12a0f7e673e3fc3bac2659ec87c84dc7c6373067
Da Vinci <105671369+SerGit-MondraHub@users.noreply.github.com>
2022-07-17 22:26:36 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const bod = document.body.querySelector(header);
+
+const welc= document.body.getElementById('welcome');
+
+function hide
+    {document.body.hidden(document.body);}
+
+welc.onmouseover= hide;
