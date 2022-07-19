9ca92210e4a7015ce19fc64e1524b6624da3808d
Da Vinci <105671369+SerGit-MondraHub@users.noreply.github.com>
2022-07-18 23:11:47 -0500

    safe welcome

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 document.getElementById('front').hidden=true;
 
 
-// let intro= document.getElementById('welcome');
+ let intro= document.getElementById('welcome');
 
 
-// intro.onclick= function()
-//     {
-//         //document.getElementById('welcome').hidden=true;
-//         document.body.removeChild(document.getElementById('welcome'));
-//     }
+ intro.onclick= function()
+     {
+         //document.getElementById('welcome').hidden=true;
+         document.body.removeChild(document.getElementById('welcome'));
+    }
