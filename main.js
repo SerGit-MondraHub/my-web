eaa713d59b14950ab4e79d72164da12fc58c4663
Da Vinci <105671369+SerGit-MondraHub@users.noreply.github.com>
2022-07-26 01:19:54 -0500

    anchors done

--- main.js
+++ main.js
@@ -14,13 +14,26 @@ enter.parentNode.onclick= function()
         document.getElementById('front-page').style.display='block';
     }
 
-//FRONT PAGE EVENTS...............
+//FRONT PAGE EVENTS....................   
+        
+//TRIGGERS ITERATION:
+for (let i=0; i<document.getElementsByTagName('section').length; i++)
+    {
+        //SUBHEADING TRIGGERS
+        let section;
+        section=document.getElementsByTagName('section')[i];//This makes the "SECTION" variable become each one of the sections that contain a subheading and a paragraph
+
+        //NAVIGATION TRIGGERS
+        let links;
+        links=document.getElementsByClassName('link')[i];//This makes the "LINKS" variable become each one of the anchors in the nav bar
+        dropdown(section,links);
+    }
 
 //DROPDOWN FUNCTION:
-function eventA(subheads,anchors)
-    {//The first child element of each SECTION is the subheading, the second one is the paragraph
+function dropdown(subheads,anchors)
+    {
         function drop()
-        {
+        {//TO SHOW THE CONTENT OF THE SUBHEADINGS
             if(subheads.children[1].style.display==='inline-block')
                 {
                     subheads.children[1].style.display='none';
@@ -33,44 +46,5 @@ function eventA(subheads,anchors)
         //EVENT LISTENERS (triggers)
         subheads.children[0].addEventListener('click',drop);
         anchors.addEventListener('click',drop);
-        
     }
-        
-        
-//SUBHEADINGS DROPDOWN:
-
-for (let i=0; i<document.getElementsByTagName('section').length; i++)
-    {
-        //SUBHEADING TRIGGERS
-        let section;
-        section=document.getElementsByTagName('section')[i];//This makes the "SECTION" variable become each one of the sections that contain a subheading and a paragraph
-        //NAVIGATION TRIGGERS
-        let links;
-        links=document.getElementsByTagName('a')[i];
-        eventA(section,links);
-
-        
-
-    }
-
-/*SAME LOGIC WITH NAV ACHORS
-
-for (let i=0; i<document.getElementsByTagName('a').length; i++)
-    {
-        let anchor;
-
-        anchor=document.getElementsByTagName('a')[i];//
-
-        anchor.onclick= function()
-            {//The first child element of each SECTION is the subheading, the second one is the paragraph
-                if(section.children[1].style.display==='inline-block')
-                    {
-                        section.children[1].style.display='none';
-                    }
-                else
-                    {
-                        section.children[1].style.display='inline-block';
-                    }
-            }
-    }
-*/
+   
